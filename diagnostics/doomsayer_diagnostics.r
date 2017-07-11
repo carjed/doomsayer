353397759526320886d9c2668d8cc0edf2fdb340
Jed Carlson <jedidiah@umich.edu>
2017-07-10 20:45:14 -0400

    Mon Jul 10 20:45:14 2017: bugfix

--- diagnostics/doomsayer_diagnostics.r
+++ diagnostics/doomsayer_diagnostics.r
@@ -13,7 +13,7 @@ packages <- c("rmarkdown", "knitr")
 package.check <- lapply(packages, FUN = function(x) {
     if (!require(x, character.only = TRUE)) {
         install.packages(x, dependencies = TRUE)
-        library(x, character.only = TRUE)
+        library(x, character.only = TRUE, silent=TRUE)
     }
 })
 
