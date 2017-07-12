6e944833da0d1fead25c20a3ab4ca80a87483b48
Jed Carlson <jedidiah@umich.edu>
2017-07-11 20:38:01 -0400

    Tue Jul 11 20:38:01 2017: debug

--- doomsayer.py
+++ doomsayer.py
@@ -210,6 +210,7 @@ elif args.input.lower().endswith('m_regions.txt'):
         usecols=(0,))
 
     M_out = np.zeros((len(samples), len(M_colnames)))
+    eprint(M_out.shape)
     for mfile in file_list:
         samples = np.loadtxt(mfile,
             dtype='S16',
@@ -218,9 +219,9 @@ elif args.input.lower().endswith('m_regions.txt'):
             usecols=(0,))
 
         M_it = np.loadtxt(mfile, skiprows=1, usecols=range(1,len(M_colnames)))
-        M_it = np.concatenate((np.array([samples]).T, M_it), axis=1)
         M_out = np.add(M_out, M_it)
 
+    M_out = np.concatenate((np.array([samples]).T, M_out), axis=1)
     M = np.delete(M_out, 0, 0)
     M = np.delete(M, 0, 1)
     M = M.astype(np.float)
